afefeb51d75bec491b15de2584ebc033975b4135
ReutKishon <reki8611@gmail.com>
2024-06-26 09:29:22 +0300

    style Welcome screen

--- client/src/screens/Login/Welcome.tsx
+++ client/src/screens/Login/Welcome.tsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, ImageBackground, View ,Text} from "react-native";
 import { Button } from "react-native-paper";
+import welcomeBackground from '../../assets/images/backgroundTwoDogs.png';
 
 const Welcome = ({ navigation }) => {
   return (
-    <View className="flex flex-col justify-center items-center h-full gap-2 w-full overflow-hidden">
-      <View className="">
+    <ImageBackground
+    source={welcomeBackground}
+    style={{justifyContent: "center",alignItems: "center",  width: '100%',height: '100%'}}
+    imageStyle={{ resizeMode: "cover",transform: [{scale:1.1},{translateY:4}],width: '100%',height: '100%'
+  }}
+  >
+    <View className="flex flex-col justify-center items-center gap-3 h-full mt-40 w-full">
         <Button
           mode="contained"
           onPress={() => {
             navigation.navigate("Register");
           }}
+          style={{ width: 270 ,borderWidth:2 }}
         >
-          Register
+          <Text className="font-semibold text-sm">Register</Text>
         </Button>
-      </View>
-      <View>
         <Button
           mode="outlined"
           onPress={() => {
             navigation.navigate("SignIn");
           }}
+          style={{ width: 270 ,backgroundColor:"white" ,borderWidth:2 , borderColor:"white"}}
         >
-          Sign In
+           <Text className="font-semibold text-sm">Sign In</Text>
         </Button>
-      </View>
     </View>
+    </ImageBackground>
   );
 };
 
